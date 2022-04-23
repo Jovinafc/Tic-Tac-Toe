44085d5d954d2af8aaad56f1c7bff1fca3c2fe69
unknown <dsouza.jovin65@gmail.com>
2022-04-23 13:12:24 +0530

    tic-tac

---
+++ src/components/Board/Board.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Cell } from '../Cell/Cell';
+import './Board.css';
+
+export const Board = ({ cellValues, winningCombination, cellClicked }) => {
+  const cells = cellValues.map((value, index) => {
+    const canHighlight =
+      winningCombination && winningCombination.indexOf(index) >= 0;
+    return (
+      <Cell
+        key={index}
+        value={value}
+        canHighlight={canHighlight}
+        onClick={() => cellClicked(index)}
+      />
+    );
+  });
+
+  return (
+    <>
+      <div className='board'>{cells}</div>
+    </>
+  );
+};
---
+++ src/components/ResultModal/ResultModal.js
@@ -0,0 +1,28 @@
+import classNames from 'classnames';
+import React from 'react';
+import './ResultModal.css';
+
+export const ResultModal = (props) => {
+  const resultModalClass = classNames({
+    'modal-open': props.isGameOver,
+  });
+
+  const message = props.winner ? `Winner is ${props.winner}` : 'It is a tie';
+
+  return (
+    <div id='modal-overlay' className={resultModalClass}>
+      <div id='game-result-modal'>
+        <div id='result-container'>
+          <div id='winner-container'>
+            <span>{message}</span>
+          </div>
+        </div>
+        <div id='new-game-container'>
+          <button id='new-game-button' onClick={props.onNewGameClicked}>
+            Start New Game
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
